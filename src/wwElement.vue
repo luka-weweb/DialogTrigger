5f79a9eae110892432b31319916fde7b203158ae
luka-weweb <luka@weweb.io>
2024-09-19 16:25:16 +0200

    updates

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,38 +1,14 @@
 <script>
 export default {
-  inject: ["activeTabProvided", "setActiveTab", "registerTabTrigger"],
   props: {
     content: { type: Object, required: true },
     wwFrontState: { type: Object, required: true },
     wwEditorState: { type: Object, required: true },
   },
-  computed: {
-    isSelected() {
-      return this.activeTabProvided === this.content.name;
-    },
-  },
-  methods: {
-    handleFocus() {
-      if (this.content.activation === "auto") {
-        this.setActiveTab(this.content.name);
-      }
-    },
-  },
-  mounted() {
-    this.registerTabTrigger(this.content.name, this.$el);
-  },
+  c,
 };
 </script>
 
 <template>
-  <wwLayoutItemContext is-repeat :index="1" :data="{ active: isSelected }">
-    <button
-      role="tab"
-      :aria-selected="isSelected"
-      @click="setActiveTab(content.name)"
-      @focus="handleFocus"
-    >
-      <wwLayout path="slot" />
-    </button>
-  </wwLayoutItemContext>
+  <wwLayout path="slot" />
 </template>
--- ww-config.js
+++ ww-config.js
@@ -1,31 +1,11 @@
 export default {
   editor: {
     label: {
-      en: "My Element",
+      en: "Trigger",
     },
     icon: "view-grid",
   },
   properties: {
-    name: {
-      label: {
-        en: "Name",
-      },
-      type: "Text",
-      bindable: true,
-      defaultValue: "",
-    },
-    activation: {
-      label: "Appearance",
-      type: "TextSelect",
-      options: {
-        options: [
-          { label: "Automatic", value: "auto", default: true },
-          { label: "Manual", value: "manual" },
-        ],
-      },
-      bindable: true,
-      defaultValue: "",
-    },
     slot: {
       hidden: true,
       defaultValue: [],
