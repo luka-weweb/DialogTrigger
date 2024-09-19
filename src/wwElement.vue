77f826118542fe9d0eea2a7fae3ccf5f25cacb23
luka-weweb <luka@weweb.io>
2024-09-19 16:51:02 +0200

    test

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -4,6 +4,7 @@ export default {
     content: { type: Object, required: true },
     wwFrontState: { type: Object, required: true },
     wwEditorState: { type: Object, required: true },
+    randomProp: { type: String, required: false },
   },
 };
 </script>
